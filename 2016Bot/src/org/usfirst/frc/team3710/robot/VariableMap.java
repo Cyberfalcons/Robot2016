cc5021b6efd072531a9e449ef7bb35f3ec450b53
Annie White <dangeroussocks22@gmail.com>
2016-02-08 14:59:12 -0500

    initial robot code

---
+++ 2016Bot/src/org/usfirst/frc/team3710/robot/VariableMap.java
@@ -0,0 +1,86 @@
+package org.usfirst.frc.team3710.robot;
+
+//Used to layout all ports and interfaces that the robot uses so that you do not put two systems on one PWM or something!
+public class VariableMap {
+
+	/*
+	 * PWM Ports (motor outputs)
+	 * 
+	 * Port 0 - Drive Left Talon
+	 * Port 1 - Drive Right Talon
+	 * Port 2 -  
+	 * Port 3 -  
+	 * Port 4 - 
+	 * Port 5 - 
+	 * Port 6 - 
+	 * Port 7 - 
+	 * Port 8 - 
+	 * Port 9 - 
+	 */
+	public static final int PWM_DRIVE_LEFT = 0;
+	public static final int PWM_DRIVE_RIGHT = 1;
+	
+	/*
+	 * PCM Modules
+	 * Port 0 - 
+	 * Port 1 - 
+	 * Port 2 - 
+	 * Port 3 - 
+	 * Port 4 -
+	 * Port 5 -
+	 * Port 6 -
+	 * Port 7 -
+	 */
+
+	
+
+	/*
+	 * Digital IO Pins
+	 * 
+	 * Port 0 - Drive Encoder Left A
+	 * Port 1 - Drive Encoder Left B
+	 * Port 2 - Drive Encoder Right A
+	 * Port 3 - Drive Encoder Right B
+	 * Port 4 - 
+	 * Port 5 -  
+	 * Port 6 - 
+	 * Port 7 - 
+	 * Port 8 - 
+	 * Port 9 - 
+	 */
+	public static final int DIO_DRIVE_ENC_LEFT_A = 0;
+	public static final int DIO_DRIVE_ENC_LEFT_B = 1;
+	public static final int DIO_DRIVE_ENC_RIGHT_A = 2;
+	public static final int DIO_DRIVE_ENC_RIGHT_B = 3;
+
+	
+	/*
+	 * Analog Pins
+	 * 
+	 * Port 0 - 
+	 * Port 1 - 
+	 * Port 2 - 
+	 * Port 3 - 
+	 */
+	
+	/* Power Channels
+	 * 
+	 * Port 0 - 
+	 * Port 1 - 
+	 * Port 2 - 
+	 * Port 3 - 
+	 * Port 4 -
+	 * Port 5 - 
+	 * Port 6 -
+	 * Port 7 -
+	 * Port 8 -
+	 * Port 9 - 
+	 * Port 10 - 
+	 * Port 11 -
+	 * Port 12 -
+	 * Port 13 -
+	 * Port 14 -
+	 * Port 15 - 
+	 */
+
+}
